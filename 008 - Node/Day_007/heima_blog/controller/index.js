f7215dc425cd5b4534a627eb8cb006c3c65e9620
liwenyao <liwenyao@itcast.cn>
2019-07-05 05:03:50 +0800

    review

--- 008 - Node/Day_007/heima_blog/controller/index.js
+++
@@ -1,9 +0,0 @@
-// 展示首页页面
-const showIndexPage = (req, res) => {
-    // 使用 render 函数之前，一定要保证安装和配置了 ejs 模板引擎
-    res.render('index.ejs', { isLogin: req.session.isLogin, user: req.session.user })
-}
-
-module.exports = {
-    showIndexPage
-}
--- 008 - Node/Day_007/heima_blog/router/article.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// 导入 用户相关的 处理函数模块
-const ctrl = require('../controller/article.js')
-router.get('/article/add', ctrl.showArticlePage)
-module.exports = router;
